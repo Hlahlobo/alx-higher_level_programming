1befd2a6801ef4a7b253f03f26e7aadafd352eb4
Hlahlobo <hfrakuoane@gmail.com>
2022-12-05 08:09:38 -0800

    3

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 4-print_hexa.py
-#Rakuoane HF <hfrakuoane@gmail.com>
+# Rakuoane HF <hfrakuoane@gmail.com>
 
 """Print numbers 0 to 98 in decimal and hexadecimal."""
 for number in range(0, 99):
-     print("{} = {}".format(number, hex(number)))
+    print("{} = {}".format(number, hex(number)))
